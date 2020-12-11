a9d9df43a8480c2355aaa94703e99c8b1bf53785
Joshua Hallee <31211978+joshuaHallee@users.noreply.github.com>
2020-12-10 22:48:10 -0500

    modify options

--- app.js
+++ app.js
@@ -14,13 +14,15 @@ options = {
 }
 
 options2 = {
-    inputFile: './output/productIds.json',
+    inputFile: `${options.outputFile}`,
     outputFile: './output/array.json'
 }
 
 options3 = {
     outputFile: `./cached/${Date.now()}.json`
 }
+
+console.log(options2)
 
 // TODO fix promises so furl.formatUrl() can run after fxml.formatXml() without error
 // fxml.formatXml(options)
@@ -29,7 +31,7 @@ options3 = {
 // TODO fix errors
 // www prefix gives error
 // any non-json/xml page returns 1
-scrape.scrapePage('https://joshuahallee.com', options3)
+// scrape.scrapePage('https://joshuahallee.com', options3)
 
 // practical result
-// scrape.scrapePage('https://api.bazaarvoice.com/data/batch.json?passkey=caHrD6xPptQbU2TrQnC3fr4YiOAMuEUcjoNxxiBFYwL3o&apiversion=5.5&displaycode=5639-en_us&resource.q0=products&filter.q0=id%3Aeq%3A1629476&stats.q0=questions%2Creviews&filteredstats.q0=questions%2Creviews&filter_questions.q0=contentlocale%3Aeq%3Aen_US&filter_answers.q0=contentlocale%3Aeq%3Aen_US&filter_reviews.q0=contentlocale%3Aeq%3Aen_US&filter_reviewcomments.q0=contentlocale%3Aeq%3Aen_US&resource.q1=questions&filter.q1=productid%3Aeq%3A1629476&filter.q1=contentlocale%3Aeq%3Aen_US&sort.q1=lastapprovedanswersubmissiontime%3Adesc&stats.q1=questions&filteredstats.q1=questions&include.q1=authors%2Cproducts%2Canswers&filter_questions.q1=contentlocale%3Aeq%3Aen_US&filter_answers.q1=contentlocale%3Aeq%3Aen_US&limit.q1=10&offset.q1=0&limit_answers.q1=10&resource.q2=reviews&filter.q2=isratingsonly%3Aeq%3Afalse&filter.q2=productid%3Aeq%3A1629476&filter.q2=contentlocale%3Aeq%3Aen_US&sort.q2=submissiontime%3Adesc&stats.q2=reviews&filteredstats.q2=reviews&include.q2=authors%2Cproducts%2Ccomments&filter_reviews.q2=contentlocale%3Aeq%3Aen_US&filter_reviewcomments.q2=contentlocale%3Aeq%3Aen_US&filter_comments.q2=contentlocale%3Aeq%3Aen_US&limit.q2=8&offset.q2=0&limit_comments.q2=3', options3)
+scrape.scrapePage('https://api.bazaarvoice.com/data/batch.json?passkey=caHrD6xPptQbU2TrQnC3fr4YiOAMuEUcjoNxxiBFYwL3o&apiversion=5.5&displaycode=5639-en_us&resource.q0=products&filter.q0=id%3Aeq%3A1629476&stats.q0=questions%2Creviews&filteredstats.q0=questions%2Creviews&filter_questions.q0=contentlocale%3Aeq%3Aen_US&filter_answers.q0=contentlocale%3Aeq%3Aen_US&filter_reviews.q0=contentlocale%3Aeq%3Aen_US&filter_reviewcomments.q0=contentlocale%3Aeq%3Aen_US&resource.q1=questions&filter.q1=productid%3Aeq%3A1629476&filter.q1=contentlocale%3Aeq%3Aen_US&sort.q1=lastapprovedanswersubmissiontime%3Adesc&stats.q1=questions&filteredstats.q1=questions&include.q1=authors%2Cproducts%2Canswers&filter_questions.q1=contentlocale%3Aeq%3Aen_US&filter_answers.q1=contentlocale%3Aeq%3Aen_US&limit.q1=10&offset.q1=0&limit_answers.q1=10&resource.q2=reviews&filter.q2=isratingsonly%3Aeq%3Afalse&filter.q2=productid%3Aeq%3A1629476&filter.q2=contentlocale%3Aeq%3Aen_US&sort.q2=submissiontime%3Adesc&stats.q2=reviews&filteredstats.q2=reviews&include.q2=authors%2Cproducts%2Ccomments&filter_reviews.q2=contentlocale%3Aeq%3Aen_US&filter_reviewcomments.q2=contentlocale%3Aeq%3Aen_US&filter_comments.q2=contentlocale%3Aeq%3Aen_US&limit.q2=8&offset.q2=0&limit_comments.q2=3', options3)
